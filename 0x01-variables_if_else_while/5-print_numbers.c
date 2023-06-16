7eb452e0af4c33d3ba221302d456490202715101
stackj007 <agamal0077@gmail.com>
2023-06-16 21:31:47 +0300

    print number of single digit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - Entry point of the program
+*
+* Description: Print single-digit numbers of base 10
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int number;
+for (number = 0; number < 10; number++)
+putchar(number + '0');
+putchar('\n');
+return (0);
+}
