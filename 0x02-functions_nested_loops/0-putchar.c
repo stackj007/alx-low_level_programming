4325f0a34050a7a4e449f65915ca583ffa0dc5cb
stackj007 <agamal0077@gmail.com>
2023-06-21 21:00:25 +0300

    0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,16 @@
-#include <stio.h>
-int main()
+#include "main.h"
+
+/**
+* print_alphabet - Prints the alphabet in lowercase.
+*/
+}
+int main (void)
 {
-printf("-putchar\n");
-return 0;
+char str[] = "_putchar";
+int ch;
+
+for(ch = 0; ch < 8; ch++)
+_putchar(str[ch]);
+_putchar('\n');
+return (0);
 }
