737bb6346f5e16d8d642f98cc72922a3de7920c4
stackj007 <agamal0077@gmail.com>
2023-07-11 21:44:49 +0300

    multipliy two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include "stdlib.h"
+#include "stdio.h"
+
+/**
+* main - Multiplies two numbers
+* @argc: Argument count
+* @argv: Argument vector
+* Return: Always 0 (Success)
+*/
+
+
+int main(int argc, char *argv[])
+{
+if (argc != 3)
+{
+printf("Error\n");
+return (1);
+}
+
+printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+return (0);
+}
